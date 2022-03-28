b5ff23006b0e3c34e0eec40fd798a18a19b65ede
Jelco van der Straaten <jelcovanderstraaten@gmail.com>
2022-03-28 21:17:48 +0200

    poah heb lang over dit kleine deel gedaan

--- src/main/java/provo/vraag/juistOnjuistVraag.java
+++ src/main/java/provo/vraag/juistOnjuistVraag.java
@@ -1,18 +1,23 @@
 package provo.vraag;
 
-import provo.vraag.Vraag;
+import java.util.Objects;
 
 public class juistOnjuistVraag implements Vraag {
     private String vraag;
-    private boolean antwoord;
+    private boolean correctAntwoord;
 
     public juistOnjuistVraag(String vraag, boolean antwoord) {
         this.vraag = vraag;
-        this.antwoord = antwoord;
+        this.correctAntwoord = antwoord;
     }
 
     @Override
     public void getVraag() {
         System.out.println("Vraag: " + vraag);
+    }
+
+    @Override
+    public boolean checkAntwoord(String antwoord) {
+        return Objects.equals(antwoord, correctAntwoord);
     }
 }
